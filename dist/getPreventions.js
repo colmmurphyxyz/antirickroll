d318d06fb62885cde1aec798d16808df6c58dc46
Colm Murphy <65133392+colmmurphyxyz@users.noreply.github.com>
2021-05-29 19:22:24 +0100

    Delete dist directory

--- dist/getPreventions.js
+++
@@ -1,5 +0,0 @@
-let preventions;
-chrome.storage.sync.get("rickRollPreventions", (items) => {
-    preventions = items.rickRollPreventions;
-    document.getElementById("foobar").innerHTML = "Prevented " + preventions + " RickRoll attempts";
-});
--- dist/rickRollWarning_onTabCreate.js
+++
@@ -1,8 +0,0 @@
-if (window.confirm("The link you just clicked is a RickRoll, are you sure you want to proceed?")) {
-    console.log("user clicked confirm");
-}
-else {
-    chrome.tabs.getCurrent((tab) => {
-        chrome.tabs.remove(tab.id);
-    });
-}
